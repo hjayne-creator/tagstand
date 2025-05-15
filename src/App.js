51317c31b1908c6509c9862ac310f8122933d10a
Houston Jayne <hjayne2@yahoo.com>
2025-05-14 19:12:01 -0700

    updates

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TagstandModernEcommerce from './components/TagstandModernEcommerce';
+
+function App() {
+  return (
+    <div className="App">
+      <TagstandModernEcommerce />
+    </div>
+  );
+}
+
+export default App;
